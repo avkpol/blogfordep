baebeb360c0568ba192b100c186226e31222fbfd
Andriy <andriykrav@gmail.com>
2015-11-20 20:27:20 +0200

    ready for heroku

--- qblog/wsgi.py
+++ qblog/wsgi.py
@@ -6,11 +6,12 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/dev/howto/deployment/wsgi/
 """
-<body>
-        <h1>Hello world!</h1>
-</body>
+
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "qblog.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
+
+from whitenoise.django import DjangoWhiteNoise
+application = DjangoWhiteNoise(application)
